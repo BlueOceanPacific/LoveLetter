3c62c82a499f546a271d35f191faaf878a9e4351
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-05-06 17:48:29 -0500

    add email input field

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function SignUp() {
-  return (<div>Sign Up!</div>);
+  return (
+    <div className="mb-3">
+      <label htmlFor="exampleFormControlInput1" className="form-label">
+        Email address
+      </label>
+      <input
+        type="email"
+        className="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+  );
 }
 
 export default SignUp;
