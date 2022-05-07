0da8983924a204a56fbf7ea350c86ca60f474881
Tom Wheeler <ttwheelerb@gmail.com>
2022-05-07 14:44:01 -0500

    implement leaderboard route with mock data

--- server/routes/baseRoutes.js
+++ server/routes/baseRoutes.js
@@ -1,5 +1,13 @@
 module.exports = function (app) {
-  app.get('/create', (req, res) => {
-    res.send('In home routes');
+  app.get('/leaderboards', (req, res) => {
+    res.send([{ username: 'KingDare', gamesPlayed: 7, gamesWon: 7 },
+      { username: 'twheel49334', gamesPlayed: 6, gamesWon: 1 },
+      { username: 'steve', gamesPlayed: 23, gamesWon: 5 },
+      { username: 'bob', gamesPlayed: 16, gamesWon: 12 },
+      { username: 'jim', gamesPlayed: 4, gamesWon: 0 },
+      { username: 'mark', gamesPlayed: 33, gamesWon: 8 },
+      { username: 'eliza', gamesPlayed: 78, gamesWon: 35 },
+      { username: 'jenny', gamesPlayed: 13, gamesWon: 10 },
+    ]);
   });
 };
