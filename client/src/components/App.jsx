884ca93e0261d4f092e203831e6cebb3585b56b6
Nick Sooter <nick.sooter@gmail.com>
2022-05-07 13:02:03 -0600

    auto-join btn, join game table

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,7 @@ import Home from './Home.jsx';
 import Chat from './Chat.jsx';
 import CreateGame from './CreateGame.jsx';
 import GameView from './GameView.jsx';
-import JoinGame from './JoinGame.jsx';
+import JoinGame from './JoinGame/JoinGame.jsx';
 import Leaderboard from './Leaderboard.jsx';
 import Lobby from './Lobby.jsx';
 import Login from './Login.jsx';
--- client/src/components/JoinGame/JoinGame.jsx
+++ client/src/components/JoinGame/JoinGame.jsx
@@ -3,12 +3,42 @@ import axios from 'axios';
 
 function JoinGame() {
   return (
-    <div classNane="container">
+    <div className="container-fluid">
       <div className="row">
-        <div className="col">Column One</div>
-        <div className="col">Column Two </div>
+        <div className="col-sm-4"><div className="p-4"></div><button type="button" className="btn btn-success btn-lg">Auto-Join</button></div>
+        <div className="col-sm-8"><div className="p-4"><h1>List of Open Games</h1></div></div>
       </div>
-    </div>
+
+      <div className="row">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col" colSpan="1">Room</th>
+              <th scope="col" colSpan="2">Current Number of Players</th>
+              <th scope="col">Join</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Friends of Sir Dare the Fair (Url)</th>
+              <td colSpan="2">3/4</td>
+              <td><button type="button" className="btn btn-primary btn-sm">Join</button></td>
+            </tr>
+            <tr>
+              <th scope="row">Friends of Sir Dareitus the Fairitus (Url)</th>
+              <td colSpan="2">1/4</td>
+              <td><button type="button" className="btn btn-primary btn-sm">Join</button></td>
+            </tr>
+            <tr>
+              <th className="text-danger" scope="row">Stupid Peasants (BANNED)</th>
+              <td colSpan="2">0/4</td>
+              <td><button type="button" className="btn btn-primary btn-sm">Join</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div >
   );
 }
 
