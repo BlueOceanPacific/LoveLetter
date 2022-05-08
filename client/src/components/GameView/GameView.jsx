cedf59a3d84713372d46ef7f3fc3f71876c68a06
Matt Teran <matthew.a.teran@gmail.com>
2022-05-08 13:50:16 -0500

    Adds rules to a modal which is opened by a button

--- client/src/components/GameView/GameView.jsx
+++ client/src/components/GameView/GameView.jsx
@@ -22,55 +22,87 @@ function GameView() {
 
   const style = { height: '100vh', width: '100vw' };
   return (
-    <div style={style} className="gameview container">
-      <div className="row justify-content-between align-items-center top-row">
-        <div className="col-3 leaderboard">
-          <LocalLeaderboard />
-        </div>
-        <div className="col">
-          <div className="row justify-content-center">
-            <div className="col-3 hand">
-              <OpponentsHand />
+    <>
+      <div className="modal" tabIndex="-1">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
             </div>
-          </div>
-        </div>
-        <div className="col-3 card-count">
-          <CardCount />
-        </div>
-      </div>
-      <div className="row bottom-row">
-        <div className="col-3 chat">Chat</div>
-        <div className="col">
-          <div className="row justify-content-between align-items-center">
-            <div className="col">
-              <div className="row justify-content-evenly">
-                <div className="col-3 hand">
-                  <OpponentsHand />
-                </div>
-                <div className="col-3 hand">
-                  <OpponentsHand />
-                </div>
-              </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
             </div>
-            <div className="col-4 rules">
-              <Rules />
-            </div>
-          </div>
-          <div className="row justify-content-between align-items-center">
-            <div className="col">
-              <div className="row justify-content-center">
-                <div className="col-5 my-hand">
-                  <MyHand />
-                </div>
-              </div>
-            </div>
-            <div className="col-4 gy-3 scoreboard">
-              <Scoreboard />
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div style={style} className="bg-light gameview">
+        <div className="row justify-content-between align-items-center top-row">
+          <div className="col-3 leaderboard">
+            <LocalLeaderboard />
+          </div>
+          <div className="col">
+            <div className="row justify-content-center">
+              <div className="col-3 hand">
+                <OpponentsHand />
+              </div>
+            </div>
+          </div>
+          <div className="col-3 card-count">
+            <Scoreboard />
+          </div>
+        </div>
+        <div className="row bottom-row">
+          <div className="col-3 chat">Chat</div>
+          <div className="col">
+            <div className="row justify-content-between align-items-center">
+              <div className="col">
+                <div className="row justify-content-evenly">
+                  <div className="col-3 hand">
+                    <OpponentsHand />
+                  </div>
+                  <div className="col-3 hand">
+                    <OpponentsHand />
+                  </div>
+                </div>
+              </div>
+              <div className="col-4 rules">
+                <CardCount />
+              </div>
+            </div>
+            <div className="row justify-content-between align-items-center">
+              <div className="col">
+                <div className="row justify-content-center">
+                  <div className="col-5 my-hand">
+                    <MyHand />
+                  </div>
+                </div>
+              </div>
+              <div className="col-4 gy-3 scoreboard">
+                <Rules />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
