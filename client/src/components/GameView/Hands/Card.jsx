66107ad10d6aad1c2ce0455bc86598302d3f0087
Matt Teran <matthew.a.teran@gmail.com>
2022-05-09 16:21:05 -0500

    connects card effects to store + changes opponents cards to render BackCard.png

--- client/src/components/GameView/Hands/Card.jsx
+++ client/src/components/GameView/Hands/Card.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Card() {
   return (
-    <div className="card" style={{ width: 'auto', height: '100%', background: '#000' }}>
+    <div className="card" style={{ width: '50%' }}>
       <li className="list-group-item" style={{ padding: 0 }}>
         <button
           type="button"
           className="list-group-item list-group-item-action"
-          style={{ padding: 0, aspectRatio: '2.2 / 3.43', width: "5.5vw" }}
+          style={{ padding: 0 }}
         >
-          {/* <img
-            src="/images/cards/Clown_Card.png"
+          <img
+            src="/images/cards/BackCard.png"
             className="card-img-top card-art-image"
             alt="..."
             style={{ height: '100%' }}
-          /> */}
+          />
         </button>
       </li>
     </div>
