49a1e842cc9fb487e6560f4eaae2c4cda0a6f44c
Matt Teran <matthew.a.teran@gmail.com>
2022-05-09 15:34:47 -0500

    Updates opponents cards

--- client/src/components/Store/store.js
+++ client/src/components/Store/store.js
@@ -1,7 +1,15 @@
 import create from 'zustand';
 
 const useStore = create((set) => ({
-  user: null,
+  user: {
+    "username": "twheeler",
+      "password": "twheeler",
+        "email": "twheeler@tw.com",
+          "pronouns": "Your Grace",
+            "avatar": "../client/dist/images/avatars/redHatCat.png",
+              "gamesPlayed": 37,
+                "gamesWon": 30,
+    },
   loggedIn: false,
   logIn: (user) => set({ user, loggedIn: true }),
   logOut: () => set({ user: null, loggedIn: false }),
