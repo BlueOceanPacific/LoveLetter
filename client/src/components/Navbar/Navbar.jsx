34f9c1a10de9d2fca8c8a65df5c6b4f11ceb30a3
Lucas <lucasbonner@protonmail.com>
2022-05-10 14:31:13 -0500

    lines up navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useStore from '../Store/store';
+import "./Navbar.scss"
 
 function Navbar() {
   const logOut = useStore(state => state.logOut);
@@ -65,37 +66,43 @@ function Navbar() {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">❤️ Love Letter</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <div className="nav-container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">❤️ Love Letter</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home</a>
-          </li>
-          {logInDisplay}
-          <li className="nav-item">
-            <a className="nav-link" href="./#/leaderboard">Leaderboard</a>
-          </li>
-          {gameDisplay}
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-toggle="dropdown">
-              Themes
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" onClick={changeBackground}>Scenic Lake</a>
-              <a className="dropdown-item" onClick={changeBackground}>Fireflies</a>
-              <a className="dropdown-item" onClick={changeBackground}>Blue Skies</a>
-            </div>
-          </li>
-        </ul>
-        {logOutButton}
-        {signup}
-      </div>
-    </nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            {logInDisplay}
+            <li className="nav-item">
+              <a className="nav-link" href="./#/leaderboard">Leaderboard</a>
+            </li>
+            {gameDisplay}
+            <li className="nav-item dropdown dd-bttn">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-toggle="dropdown" style={{margin: 0}}>
+                Themes
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" onClick={changeBackground}>Scenic Lake</a>
+                <a className="dropdown-item" onClick={changeBackground}>Fireflies</a>
+                <a className="dropdown-item" onClick={changeBackground}>Blue Skies</a>
+              </div>
+            </li>
+            <li>
+            {logOutButton}
+            </li>
+            <li>
+              {signup}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
