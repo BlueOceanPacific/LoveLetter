c1fb3a2a41da02b85d7be7723224151539a57a7c
Matt Teran <matthew.a.teran@gmail.com>
2022-05-09 20:46:36 -0500

    implements live chat with socket io

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const app = express()
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+// ------------ Socket.io setup ---------------------
+const httpServer = require('http').createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(httpServer);
 
 // ------------ Middleware setup --------------------
-const app = express();
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 const sessionStore = new MongoDBStore({
   uri: 'mongodb://127.0.0.1/LoveLetter',
@@ -25,5 +32,19 @@ require('./routes/userRoutes')(app);// user routes
 require('./routes/baseRoutes')(app);// "base" routes like create game, get leaderboard, etc
 require('./routes/gameRoutes')(app);// routes for actual gameplay
 
-// eslint-disable-next-line no-console
-app.listen(4000, () => console.log('listening on port 4000'));
+// ----------------- Listen for Socket IO Connections -------------------------
+io.on('connection', (socket) => {
+  const room = socket.handshake.query.id;
+  socket.join(room);
+
+  socket.on('disconnect', function() {
+    socket.leave(room)
+  });
+  // on 'chat' events in a room, send chat to clients in room, excluding sender
+  socket.on('chat', (chat) => {
+    socket.broadcast.to(room).emit('chat', chat);
+  });
+})
+
+// app.listen but with socket io plugged in
+httpServer.listen(4000, () => console.log('listening on port 4000'));
