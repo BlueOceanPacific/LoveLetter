1d79872a489a79b780a3e8f2fecfd1d8c414ae1b
Marilene Soares da Costa <lene626@gmail.com>
2022-05-09 20:20:49 -0600

    adds avatar dropdown menu

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -11,42 +11,17 @@ import './UserProfile.scss';
 
 function UserProfile() {
   const user = useStore((state) => state.user);
-
-  const avatarOptions = [
-    {
-      id: '1',
-      title: "blue",
-      image: '/images/avatars/blueFlowersCat.png'
-    },
-    {
-      id: '2',
-      title: "disgusted",
-      image: '/images/avatars/disgustedCat.png'
-    },
-    {
-      id: '3',
-      title: "red",
-      image: '/images/avatars/redHatCat.png'
-    },
-    {
-      id: '4',
-      title: "scared",
-      image: '/images/avatars/underBlanketCat.png'
-    },
-    {
-      id: '5',
-      title: "void",
-      image: '/images/avatars/voidCat.png'
-    },
-  ]
-
-
+  const [showDropdown, setShowDropdown] = useState(false);
   const [data, setData] = useState({
     pronouns: user.pronouns,
     avatar: user.avatar
   })
-
   const { pronouns, avatar } = data;
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown(!isShown);
+  }
 
   const changeHandler = (e) => {
     setData({ ...data, [e.target.name]: [e.target.value] });
@@ -62,6 +37,9 @@ function UserProfile() {
       .catch((err) => console.log(err));
   };
 
+  const toggleDropdown = () => {
+    setShowDropdown = true;
+  }
 
   return (
     <div>
@@ -80,10 +58,20 @@ function UserProfile() {
 
                 <img className="avatarRound" src={`${user.avatar}`} alt="avatar" />
               </div>
-              {/* add drop down menu */}
-              {/* conditional view of dropdown? */}
-              <p>Choose your avatar:</p>
-
+              <div className="dropdown">
+                <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                  Choose your avatar
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li><img className="dd-thumbnail" src="/images/avatars/blueFlowersCat.png" /><span>Blue</span></li>
+                  <li><img className="dd-thumbnail" src="/images/avatars/redHatCat.png" />
+                    <span> Red</span></li>
+                  <li><img className="dd-thumbnail" src="/images/avatars/underBlanketCat.png" />
+                    <span>Grey</span></li>
+                  <li><img className="dd-thumbnail" src="/images/avatars/voidCat.png" />
+                    <span>Black</span></li>
+                </ul>
+              </div>
             </div>
 
             <div className="pronouns">
@@ -97,14 +85,15 @@ function UserProfile() {
             </div>
             <div className="edit">
               {/* cancel clears form - new get? */}
-              <button type="submit" className="btn btn-primary UP-edit-btn"> Cancel</button>
+              <button type="button" data-bs-target="#" className="btn btn-primary UP-edit-btn">Cancel</button>
+
               <button type="submit" name="submit" className="btn btn-primary UP-edit-btn"> Save</button>
             </div>
           </form>
         </div>
         <div className="col-sm" />
       </div >
-    </div >
+    </div>
   );
 }
 
