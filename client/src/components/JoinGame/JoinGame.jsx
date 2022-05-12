84bf4e40becf6ca89a025cc5e16f6b6d9879aa4b
Nick Sooter <nick.sooter@gmail.com>
2022-05-12 08:39:47 -0600

    socket set up for JoinGame

--- client/src/components/JoinGame/JoinGame.jsx
+++ client/src/components/JoinGame/JoinGame.jsx
@@ -2,48 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import 'regenerator-runtime/runtime';
-// import io from 'socket.io-client';
-// import useStore from '../Store/store';
+import io from 'socket.io-client';
+import useStore from '../Store/store';
 
 function JoinGame() {
   const [games, setGames] = useState([]);
-  // const [socket, setSocket] = useState(io('/play', { query: { id } }));
-  // const user = useStore((state) => state.user);
+  const [socket, setSocket] = useState(io('/play', /*{ query: { id } }*/));
+  const user = useStore((state) => state.user);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getGames = async () => {
-      try {
-        const res = await axios.get('/games');
-        console.log('send get req: ', res);
-        setGames(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getGames();
-  }, []);
-
-  /*
   useEffect(() => {
     socket.emit('join', user);
     socket.on('join', () => {
       console.log('someone connected');
-      const getGames = async () => {
-      try {
-        const res = await axios.get('/games');
-        console.log('send get req: ', res);
-        setGames(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getGames();
     });
+    axios
+      .get('/games')
+      .then(({ data }) => setGames(data))
+      .catch((err) => console.log(err));
   }, []);
 
+
+
+
   // socket.emit - This method is responsible for sending messages. socket.on - This method is responsible for listening for incoming messages.
-  */
+
   const handleAutoJoin = () => {
     navigate(`/play/lobby/${games[0].name}`);
   };
@@ -97,7 +80,22 @@ function JoinGame() {
 }
 
 export default JoinGame;
-//
+
+/*
+  useEffect(() => {
+    const getGames = async () => {
+      try {
+        const res = await axios.get('/games');
+        console.log('send get req: ', res);
+        setGames(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getGames();
+  }, []);
+*/
+
 
 
 
