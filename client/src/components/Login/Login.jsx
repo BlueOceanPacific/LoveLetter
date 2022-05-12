32af60b877ac9d978bc91e94102c4d1b53502c5f
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-05-12 12:22:57 -0400

    remove space

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-
 import './Login.scss';
 import useStore from '../Store/store';
 
