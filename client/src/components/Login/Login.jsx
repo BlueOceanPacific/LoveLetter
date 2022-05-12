e5e00c42f469f47365c0a5ade3eb03ecb8bc23bf
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2022-05-12 12:18:28 -0400

    remove alerts for modal and conditional rendering

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,84 +1,103 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+
 import './Login.scss';
 import useStore from '../Store/store';
 
 function Login() {
   const logIn = useStore((state) => state.logIn);
   const navigate = useNavigate();
-
-  const forgotPassword = () => {
-    alert('Users are not able to reset passwords or change username');
-  };
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameHelper, setUsernameHelper] = useState(() => '');
 
-  const validatesLogin = () => {
+  const validateLogin = () => {
     axios
       .post('/user/login', { username, password })
       .then(({ data }) => {
-        console.log('logged in as: ', data);
         if (data.user) {
           logIn(data.user);
         }
         return navigate('/');
       })
-      .catch((err) => {
-        console.log(err);
-        alert('Invalid username or password');
+      .catch(() => {
+        document.getElementById('login-username-help').style.color = '#ff0000';
+        setUsernameHelper('the credentials you entered did not match our records.');
       });
   };
 
   return (
-    <div className="d-flex justify-content-center" id="login">
-      <h1>Welcome!</h1>
-      <br />
+    <div className="container" id="login-container">
+      <div className="d-flex justify-content-center" id="login">
+        <h1>Welcome!</h1>
 
-      <form className="form">
-        <input
-          className="input"
-          type="text"
-          placeholder="username"
-          id="login-input"
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <br />
-        <br />
-        <input
-          className="input"
-          type="text"
-          placeholder="password"
-          id="login-input"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </form>
+        <form className="form">
+          <div className="row">
+            <input
+              className="input"
+              type="text"
+              placeholder="Username"
+              id="login-input"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
 
-      <br />
-      <button
-        type="button"
-        className="btn btn-primary btn-sm"
-        id="login-btn"
-        onClick={validatesLogin}
-      >
-        Submit
-      </button>
-      <br />
-      <Link to="/signup">
-        <button type="button" className="btn btn-secondary btn-sm">
-          Sign Up
-        </button>
-      </Link>
-      <br />
-      <button
-        type="button"
-        className="btn btn-secondary btn-sm"
-        id="login-pw-btn"
-        onClick={forgotPassword}
-      >
-        Forgot Password
-      </button>
+          <div id="login-username-help" className="form-text">
+            {usernameHelper}
+          </div>
+
+          <div className="row">
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              id="password-input"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+        </form>
+
+        <div className="row">
+          <button type="button" className="btn btn-primary" id="login-btn" onClick={validateLogin}>
+            Submit
+          </button>
+        </div>
+
+        <Link to="/signup">
+          <div className="row">
+            <button type="button" className="btn btn-primary">
+              Sign Up
+            </button>
+          </div>
+        </Link>
+        <div className="row">
+          <button
+            type="button"
+            className="btn btn-primary"
+            id="login-pw-btn"
+            data-toggle="modal"
+            data-target=".bd-example-modal-sm"
+            data-bs-target="#forgotPasswordModal"
+            data-bs-toggle="modal"
+          >
+            Forgot Password
+          </button>
+        </div>
+
+        <div
+          className="modal fade bd-example-modal-sm"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="mySmallModalLabel"
+          aria-hidden="true"
+          id="forgotPasswordModal"
+        >
+          <div className="modal-dialog modal-sm modal-dialog-centered">
+            <div className="modal-content">Users are not able to reset passwords</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
